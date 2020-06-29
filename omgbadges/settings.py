3c23a5c909fe5dd6062fae0184bf332540abd704
kautukkundan <kautukkundan@gmail.com>
2020-06-29 20:48:30 +0530

    added email extraction2

--- omgbadges/settings.py
+++ omgbadges/settings.py
@@ -86,23 +86,23 @@ WSGI_APPLICATION = 'omgbadges.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   }
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'omgbadges',
-#         'USER': 'omgbadgesuser',
-#         'PASSWORD': 'omgpassword',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'omgbadges',
+        'USER': 'omgbadgesuser',
+        'PASSWORD': 'omgpassword',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 
 # Password validation
