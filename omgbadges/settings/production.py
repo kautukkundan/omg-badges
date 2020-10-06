b1201f5090d2d2be3a58d16538535e68c1fdade7
DesignrKnight <notify.abelmathew@gmail.com>
2020-10-06 16:41:21 +0530

    Use SQLite for Production

--- omgbadges/settings/production.py
+++ omgbadges/settings/production.py
@@ -7,14 +7,23 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:8080',
     'https://dscomg.com',
 ]
-
+#Default for production is set to use sqlite
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD':config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+#Use below one to use postgresql for production
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': config('DB_NAME'),
+#        'USER': config('DB_USER'),
+#        'PASSWORD':config('DB_PASSWORD'),
+#        'HOST': config('DB_HOST'),
+#        'PORT': config('DB_PORT')
+#    }
+#}
