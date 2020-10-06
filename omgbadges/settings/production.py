b2aaabfc1446e7960dc796274ee4925136cdc304
DesignrKnight <notify.abelmathew@gmail.com>
2020-10-06 17:03:05 +0530

    Change directory structure for Docker volume mount

--- omgbadges/settings/production.py
+++ omgbadges/settings/production.py
@@ -11,7 +11,7 @@ CORS_ORIGIN_WHITELIST = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'databases', 'db.sqlite3'),
     }
 }
 
